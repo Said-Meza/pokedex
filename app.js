05a015bf0222b4b3083997a1f232ea45be5b8ef2
said-meza <176p0393@itspozarica.edu.mx>
2023-09-22 20:35:53 -0600

    tercer commit

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ async function pokemons() {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon/'+ numerorandon) ;
     const pokes = await response.json();
     
-    console.log(pokes);
     
      rutas={
         name:pokes.forms[0].name,
@@ -26,13 +25,11 @@ async function pokemons() {
         // abilites2:pokes.abilities[1].ability.name
 
     };
-        // console.log(rutas.name);
         
         nombre.textContent = rutas.name;
         imagen.src =rutas.img;
         hability.textContent = `Habilidades : ${rutas.abilites1} `;
         
-        // console.log(rutas.abilites1)
 
 
     }
